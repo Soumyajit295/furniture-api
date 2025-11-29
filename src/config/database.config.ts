018db5bbe228243389beeaf8862aa5e699be5970
Soumyajit295 <soumyajit.de20190@gmail.com>
2025-11-29 14:05:42 +0530

    feat: primary setup

---
+++ src/config/database.config.ts
@@ -0,0 +1,20 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+  inject: [ConfigService],
+
+  useFactory: (config: ConfigService) => ({
+    type: 'postgres',
+    host: config.get('DB_HOST'),
+    port: Number(config.get('DB_PORT')),
+    username: config.get('DB_USER'),
+    password: config.get('DB_PASS'),
+    database: config.get('DB_NAME'),
+
+    autoLoadEntities: true,
+    synchronize: false,
+    entities: [__dirname + '/../entities/*.{ts,js}'],
+    migrations: [__dirname + '/../../migrations/*.{ts,js}'],
+  }),
+};
